51be029b5cb3336d25597a98a39227db67f3853e
Veranikl <nijakavata@gmail.com>
2023-05-15 22:05:05 +0200

    Updated list of main cities, added JS

--- javascript/index.js
+++ javascript/index.js
@@ -18,6 +18,29 @@ function showTime() {
     warsawDate.innerHTML = warsawData.format("MMMM D, YYYY");
     warsawTime.innerHTML = warsawData.format("h:mm:ss [<small>]A[</small>]");
   }
+  //Vancouver
+  let vancouverElement = document.querySelector("#vancouver");
+  if (vancouverElement) {
+    let vancouverDate = vancouverElement.querySelector(".date");
+    let vancouverTime = vancouverElement.querySelector(".time");
+    let vancouverData = moment().tz("America/Vancouver");
+    vancouverDate.innerHTML = vancouverData.format("MMMM D, YYYY");
+    vancouverTime.innerHTML = vancouverData.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+
+    //Sao Paulo
+    let saopauloElement = document.querySelector("#sao-paulo");
+    if (saopauloElement) {
+      let saopauloDate = saopauloElement.querySelector(".date");
+      let saopauloTime = saopauloElement.querySelector(".time");
+      let saopauloData = moment().tz("America/Sao_Paulo");
+      saopauloDate.innerHTML = saopauloData.format("MMMM D, YYYY");
+      saopauloTime.innerHTML = saopauloData.format(
+        "h:mm:ss [<small>]A[</small>]"
+      );
+    }
+  }
 }
 
 function updateCities(event) {
@@ -41,6 +64,10 @@ function updateCities(event) {
          <br />
           <a class="backMenu" href="index.html">All cities ⤴️</a>
         `;
+
+  setTimeout(() => {
+    updateCities(event);
+  }, 1000);
 }
 
 showTime();
